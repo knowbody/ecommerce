5953d81662f923458980521a07b041004c17aaba
Mateusz Zatorski <mateuszzatorski@gmail.com>
2014-03-22 17:47:04 +0000

    added hello world message to views

--- toyshop/urls.py
+++ toyshop/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from shop.views import hello
 
 admin.autodiscover()
 
@@ -10,6 +11,7 @@ urlpatterns = patterns('',
                        # url(r'^$', 'toyshop.views.home', name='home'),
                        # url(r'^toyshop/', include('toyshop.foo.urls')),
 
+                       url(r'^$', hello, name='hello_message'),
                        # Uncomment the admin/doc line below to enable admin documentation:
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
